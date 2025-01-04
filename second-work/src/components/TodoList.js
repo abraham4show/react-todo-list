d174eebbc4ceef5ae39c983ca55e5c0f4d1c5739
abraham4show <shorinwaafolabi@gmail.com>
2025-01-04 02:55:51 +0100

    Update TodoList.js

--- second-work/src/components/TodoList.js
+++ second-work/src/components/TodoList.js
@@ -1,12 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Item from './Todoitem';
+
 export default class Todo extends Component {
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
-      <div>
-        <h2>todo list</h2>
-        <Item />
-      </div>
-    )
+      <ul className="list-group my-5">
+        <h3 className="text-capitalize text-center">todo list</h3>
+        {items.map(item => {
+          return (
+            <Item 
+              key={item.id}
+              title={item.title}
+              handleDelete={() => handleDelete(item.id)}  // Fixed this from `this.handleDelete` to `handleDelete`
+              handleEdit={() => handleEdit(item.id)}      // Fixed this from `this.handleEdit` to `handleEdit`
+            />
+          );
+        })}
+        <button 
+          type="button" 
+          className="btn btn-danger btn-block text-uppercase mt-5" 
+          onClick={clearList}
+        >
+          Clear
+        </button>
+      </ul>
+    );
   }
 }
